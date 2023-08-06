f25f918a15655a5c57bf2cf6aef6d86fe11eae31
Eric Nichols <ebn646@gmail.com>
2023-08-06 11:56:35 -0400

    added back topic list

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import TopicsList from '@/components/TopicsList'
 export default function Home() {
   return (
     <>
-      {/* <TopicsList /> */}
-     <h1>HELLO</h1>
+      <TopicsList />
     </>
   )
 }
